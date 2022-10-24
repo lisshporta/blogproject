57f8a74a5724b2ee6526f671de3bdbaa23bdf2c2
= <=>
2022-10-24 11:08:59 +0200

    Project Update (f+b)

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -1,6 +1,5 @@
 @props(['post'])
 
-
 <article
             class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
             <div class="py-6 px-5 lg:flex">
@@ -27,7 +26,7 @@
                             </h1>
 
                             <span class="mt-2 block text-gray-400 text-xs">
-                                    Published <time>1 day ago</time>
+                                    Published <time>{{$post->created_at}}</time>
                                 </span>
                         </div>
                     </header>
