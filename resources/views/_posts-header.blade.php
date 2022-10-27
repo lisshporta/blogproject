c7248f7ef32944945616269e4cbc729aeca36a7f
= <=>
2022-10-27 14:09:00 +0200

    Project Update

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -18,21 +18,13 @@
                     <x-slot name="trigger">
                         <button class=" py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left lg:inline-flex"
                         > {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
-                
-                        <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                        height="22" viewBox="0 0 22 22">
-                       <g fill="none" fill-rule="evenodd">
-                           <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                           </path>
-                           <path fill="#222"
-                                 d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                       </g>
-                   </svg> 
+
+                    <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;" /> 
                     </button>
                     </x-slot>
 
 
-                  <x-dropdown-item href="/" :active="request()->routeIs('home')">All </x-dropdown-item>
+                  <x-dropdown-item href="/" :active="request()->routeIs()">All </x-dropdown-item>
                @foreach ($categories as $category)
                <x-dropdown-item 
                    href="/categories/{{ $category->slug }}"
