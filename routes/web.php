249b6b4b3311e1701262ca2344a89c85ba854f80
Lis Shporta <lis.shporta@gmail.com>
2022-10-28 15:29:21 +0200

    Project Update

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ Route::get('/' , [PostController::class , 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class , 'show']);
 Route::get('authors/{author:username}', function (User $author) {
   return view('posts' , [
-    'posts' => $author->posts,
-    'categories' => Category::all()
+    'posts' => $author->posts
   ]);
 });
