3a7e6d929c8bc9392e0c52889e9c6602451d4b36
Lis Shporta <lis.shporta@gmail.com>
2022-10-28 15:54:01 +0200

    Changes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 
 <title>Laravel From Scratch Blog</title>
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<script rel="preconnect" src="https://cdn.tailwindcss.com"></script>
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
