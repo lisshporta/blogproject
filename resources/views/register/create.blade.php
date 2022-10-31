028f2106f952ab4270f019895ea13a54c9ff3b27
Lis Shporta <lis.shporta@gmail.com>
2022-10-31 14:53:04 +0100

    user register update

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -17,8 +17,13 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{ old('name') }}"
                            required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -32,8 +37,13 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{ old('username') }}"
                            required
                     >
+
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
                 </div>
 
                 <div class="mb-6">
@@ -47,6 +57,7 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{ old('email') }}"
                            required
                     >
 
@@ -68,6 +79,10 @@
                            id="password"
                            required
                     >
+
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
                 </div>
 
                 <div class="mb-6">
@@ -76,8 +91,13 @@
                         >
                         Submit
                 </div>
-            
-
+                {{-- @if ($errors->any())
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li class="text-red-500 text-xs">{{ $error }}</li>
+                    @endforeach
+                </ul>    
+                @endif --}}
             </form>
         </main>
     </section>
