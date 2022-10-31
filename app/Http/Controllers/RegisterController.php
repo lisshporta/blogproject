7c4ad6bf98133bb688a211dc8b0279eef34c6eff
Lis Shporta <lis.shporta@gmail.com>
2022-10-31 15:33:09 +0100

    project update

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,7 +15,7 @@ class RegisterController extends Controller
     public function store()
     {
        $attributes = request()->validate([
-            'name' => 'required|max:255',
+            'name' => 'required|max:255|unique:users,name',
             'username' => 'required|min:3|max:255|unique:users,username',
             'email' => 'required|email|max:255|unique:users,email',
             'password' => 'required|min:8|max:255'
