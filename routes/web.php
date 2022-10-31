b421ac7d0ca905b57db46626f41569503d84f25e
Lis Shporta <lis.shporta@gmail.com>
2022-10-31 14:10:44 +0100

    User Register Page

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,11 @@ use Illuminate\Support\Facades\Route;
 use League\CommonMark\Extension\FrontMatter\Data\LibYamlFrontMatterParser;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Support\Facades\File;
+use App\Http\Controllers\RegisterController;
 
 
 Route::get('/' , [PostController::class , 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class , 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
