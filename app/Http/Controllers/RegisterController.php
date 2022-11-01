1037ad0ea86b6b162f8a7bb1541fce39f6989bce
Lis Shporta <lis.shporta@gmail.com>
2022-11-01 10:56:36 +0100

    LogIn/LogOut

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,20 +15,15 @@ class RegisterController extends Controller
 
     public function store()
     {
-       $attributes = request()->validate([
-            'name' => 'required|max:255|unique:users,name',
+        $attributes = request()->validate([
+            'name' => 'required|max:255',
             'username' => 'required|min:3|max:255|unique:users,username',
             'email' => 'required|email|max:255|unique:users,email',
-            'password' => 'required|min:8|max:255'
+            'password' => 'required|min:8|max:255',
         ]);
 
-        $attributes['password'] = bcrypt($attributes['password']);
+        auth()->login(User::create($attributes));
 
-        $user = User::create($attributes);
-
-        auth()->login($user);
-
-        return redirect('/')->with('success' , 'Your account has been created');
+        return redirect('/')->with('success', 'Your account has been created.');
     }
 }
-  
