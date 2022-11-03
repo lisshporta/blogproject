f69ab24a17aedcd70336361356332017575dde99
Lis Shporta <lis.shporta@gmail.com>
2022-11-03 16:01:37 +0100

    Project Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,30 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator;
-use Illuminate\Support\ServiceProvider;
+use App\Services\Newsletter;
+use App\Services\MailchimpNewsletter;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
+
+     
     public function register()
     {
-        //
+        
+        app()->bind(Newsletter::class, function () {
+            $client = (new ApiClient)->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us21'
+            ]);
+
+            return new MailchimpNewsletter($client);
+        });
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
+   
     public function boot()
     {
         Model::unguard();
