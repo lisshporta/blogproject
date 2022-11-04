8de6465f3a3cac4e9c9eb0b0fcfc0d9d6dba937f
Lis Shporta <lis.shporta@gmail.com>
2022-11-04 16:23:50 +0100

    Project Update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -24,12 +24,20 @@
 
             <div class="mt-8 md:mt-0 flex items-center">
                 @auth
-                    <span class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!</span>
+                    <x-dropdown>
+                        <x-slot name="trigger">
+                        <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!</button>
+                        </x-slot>
 
-                    <form method="POST" action="/logout" class="text-xs font-bold ml-6 text-blue-500">
-                        @csrf
-                        <button type="submit" class="uppercase">Log Out</button>
-                    </form>
+                        <x-dropdown-item href="/">Dashboard</x-dropdown-item>
+                        <x-dropdown-item href="/admin/posts/create">New Post</x-dropdown-item>
+                        <x-dropdown-item href="#">New Category</x-dropdown-item>
+                        <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
+                        <form id="logout-form" method="POST" action="/logout" class="hidden">
+                            @csrf
+                        </form>
+                    </x-dropdown>
+                   
                 @else
                     <a href="/register" class="text-xs font-bold uppercase">Register</a>
                     <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -20,6 +20,7 @@
 
                 <select name="category_id" id="category_id">
                     @foreach (\App\Models\Category::all() as $category)
+
                     <option 
                             value="{{ $category->id }}"
                             {{ old('category_id') == $category->id ? 'selected' : '' }}
